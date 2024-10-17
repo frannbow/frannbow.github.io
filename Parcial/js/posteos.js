b240a8b1aebffee01d59eb5fcd4f29d4929bb47e
frannbow <108700145+frannbow@users.noreply.github.com>
2024-10-17 11:42:13 -0300

    Add files via upload

---
+++ Parcial/js/posteos.js
@@ -0,0 +1,20 @@
+const posting = [
+    {
+        id:"1",
+        title: "en casa",
+        img: "/img/en-casa.jpg",
+        fecha_hora: "19/11 10:23"
+    },
+    {
+        id:"1",
+        title: "durmiendo",
+        img: "/img/durmiendo.jpg",
+        fecha_hora: "19/11 10:23"
+    },
+    {
+        id:"1",
+        title: "trabajando",
+        img: "/img/trabajando.jpg",
+        fecha_hora: "19/11 10:23"
+    },
+]
